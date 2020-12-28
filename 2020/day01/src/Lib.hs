cfea9a49c52687fd2e39ddf408f4669d70f61d98
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2020-12-28 12:00:24 -0500

    day 1 part 2

--- 2020/day01/src/Lib.hs
+++ 2020/day01/src/Lib.hs
@@ -11,11 +11,12 @@ someFunc = do
             contents <- readFile "input"
             let codes = lines contents
             let ints = map readInt codes
-            let ans = [ (i,j) | i <- ints, j <- ints, i + j == 2020 ]
-            let (x, y) = head ans
+            let ans = [ (i,j,k) | i <- ints, j <- ints, k <- ints, i + j + k == 2020 ]
+            let (x, y, z) = head ans
             print x
             print y
-            print (x*y)
+            print z
+            print (x*y*z)
 
 readInt :: String -> Int
 readInt = read
