94df727d29a1ae4f54ed8f4f110a84a20c9f6cdb
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2020-12-29 10:45:46 -0500

    day 3 part 2

--- 2020/day03/src/Lib.hs
+++ 2020/day03/src/Lib.hs
@@ -5,20 +5,23 @@ module Lib
 import System.IO
 import Control.Monad
 
---1-3 a: abcde
---1-3 b: cdefg
---2-9 c: ccccccccc
+--Right 1, down 1.
+--Right 3, down 1. (This is the slope you already checked.)
+--Right 5, down 1.
+--Right 7, down 1.
+--Right 1, down 2.
 
 someFunc :: IO ()
 someFunc = do
             contents <- readFile "input"
             let rows = lines contents
-            let len = length $ head rows
-            let ys = takeWhile (< length rows) [0,1..]
-            let xs = [0,3..]
-            let pairs = zip ys xs
-            let hits = filter (tree rows len) pairs 
-            print $ length hits
+            let slopes = [(1,1),(1,3),(1,5),(1,7),(2,1)] :: [(Int, Int)]
+            let coords = map (pairs $ length rows) slopes
+--            print coords
+            let counts = map (trees rows) coords
+            print counts
+            let mult = product counts
+            print mult
 --            print head codes
 --            let ans = [ (i,j,k) | i <- ints, j <- ints, k <- ints, i + j + k == 2020 ]
 --            let (x, y, z) = head ans
@@ -27,5 +30,17 @@ someFunc = do
 --            print z
 --            print (x*y*z)
 
+pairs :: Int -> (Int, Int) -> [(Int, Int)]
+pairs len xy =
+  let ys = takeWhile (< len) [0,fst xy..]
+      xs = [0,snd xy..]
+  in zip ys xs
+
+trees :: [String] -> [(Int,Int)] -> Int
+trees ground yxs =
+  let len = length $ head ground
+      hits = filter (tree ground len) yxs
+  in length hits
+ 
 tree :: [String] -> Int -> (Int, Int) -> Bool
 tree ground len (y, x) = (ground!!y)!!mod x len == '#'
