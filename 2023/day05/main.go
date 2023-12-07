6477e0dd5cda8c2c57713b1940ff81582b8d9c9b
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2023-12-07 21:20:40 +0000

    day 5 part 1

---
+++ 2023/day05/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// lines := strings.Split(sample, "\n")
+	lines := strings.Split(data, "\n")
+
+	seeds := []int64{}
+	seedStrings := strings.Split(strings.TrimPrefix(lines[0], "seeds: "), " ")
+	for _, i := range seedStrings {
+		i64, _ := strconv.ParseInt(i, 10, 64)
+		seeds = append(seeds, i64)
+	}
+	mappings := []*Mapping{}
+	var mapping *Mapping
+
+	for _, line := range lines[1:] {
+		if line == "" {
+			continue
+		}
+		if strings.HasSuffix(line, " map:") {
+			fromTo := strings.Split(strings.TrimSuffix(line, " map:"), "-")
+			mapping = &Mapping{source: fromTo[0], destination: fromTo[2], ranges: []Range{}}
+			mappings = append(mappings, mapping)
+		} else {
+			nums := strings.Split(line, " ")
+			dest, _ := strconv.ParseInt(nums[0], 10, 64)
+			src, _ := strconv.ParseInt(nums[1], 10, 64)
+			length, _ := strconv.ParseInt(nums[2], 10, 64)
+			mapping.ranges = append(mapping.ranges, Range{destinationStart: dest, sourceStart: src, length: length})
+		}
+	}
+
+	min := int64(math.MaxInt64)
+	for _, seed := range seeds {
+		i := seed
+		for _, mapping := range mappings {
+			i = mapping.mapTo(i)
+		}
+		if i < min {
+			min = i
+		}
+	}
+
+	fmt.Println(min)
+}
+
+type Mapping struct {
+	source      string
+	destination string
+	ranges      []Range
+}
+
+type Range struct {
+	destinationStart int64
+	sourceStart      int64
+	length           int64
+}
+
+func (m Mapping) mapTo(in int64) int64 {
+	for _, r := range m.ranges {
+		if out, match := r.mapTo(in); match {
+			return out
+		}
+	}
+	return in
+}
+
+func (r Range) mapTo(in int64) (int64, bool) {
+	if in >= r.sourceStart && in < r.sourceStart+r.length {
+		return (in - r.sourceStart) + r.destinationStart, true
+	} else {
+		return -1, false
+	}
+}
+
+const sample string = `seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4`
+
+const data string = ``
