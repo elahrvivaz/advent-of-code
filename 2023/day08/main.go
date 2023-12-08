582325f315dad41c9baf9e6858cfb6b46bca6a82
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2023-12-08 15:49:30 +0000

    day 8 part 2

--- 2023/day08/main.go
+++ 2023/day08/main.go
@@ -8,33 +8,55 @@ import (
 )
 
 func main() {
-	// lines := getInput(Sample)
-	// lines := getInput(Sample2)
-	lines := getInput(Data)
+	// lines := getInput(sample)
+	// lines := getInput(sample2)
+	lines := getInput(sample3)
+	// lines := getInput(data)
 
-	regex := regexp.MustCompile(`([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)`)
+	regex := regexp.MustCompile(`([1-9A-Z]{3}) = \(([1-9A-Z]{3}), ([1-9A-Z]{3})\)`)
 
-	nodes := make(map[string]*Node)
+	nodeMap := make(map[string]*Node)
 	dirs := lines[0]
+	cur := []*Node{}
 
 	for _, line := range lines[2:] {
 		matches := regex.FindStringSubmatch(line)
-		nodes[matches[1]] = &Node{Name: matches[1], Left: matches[2], Right: matches[3]}
+		node := &Node{Name: matches[1], LeftName: matches[2], RightName: matches[3], Terminal: matches[1][2] == 'Z'}
+		nodeMap[node.Name] = node
+		if node.Name[2] == 'A' {
+			cur = append(cur, node)
+		}
+	}
+
+	for _, n := range nodeMap {
+		n.Left = nodeMap[n.LeftName]
+		n.Right = nodeMap[n.RightName]
 	}
 
 	steps := 0
+	complete := []int{}
 
-	cur := nodes["AAA"]
-	i := 0
-	for cur.Name != "ZZZ" {
+	for i := 0; len(complete) < len(cur); {
 		steps += 1
 		if dirs[i] == 'L' {
-			fmt.Println(cur.Name + "->" + cur.Left)
-			cur = nodes[cur.Left]
+			for j := range cur {
+				// fmt.Println(cur[j].Name + "->" + cur[j].Left)
+				cur[j] = cur[j].Left
+				if cur[j].Terminal {
+					complete = append(complete, steps)
+				}
+			}
 		} else {
-			fmt.Println(cur.Name + "->" + cur.Right)
-			cur = nodes[cur.Right]
+			for j := range cur {
+				// fmt.Println(cur[j].Name + "->" + cur[j].Right)
+				cur[j] = cur[j].Right
+				if cur[j].Terminal {
+					complete = append(complete, steps)
+				}
+			}
 		}
+
+		// fmt.Println()
 		if i < len(dirs)-1 {
 			i += 1
 		} else {
@@ -42,14 +64,45 @@ func main() {
 		}
 	}
 
-	fmt.Println(steps)
+	// find LCM of the steps for each node
+	multiples := []int{}
+	multiples = append(multiples, complete...)
+
+	for i := next(multiples); i != -1; i = next(multiples) {
+		multiples[i] += complete[i]
+		// fmt.Println(multiples)
+	}
+
+	fmt.Println(multiples[0])
+}
+
+func next(mult []int) int {
+	done := true
+	min := 0
+	for i := range mult[1:] {
+		if mult[i+1] < mult[min] {
+			min = i + 1
+			done = false
+		} else if mult[i+1] != mult[min] {
+			done = false
+		}
+	}
+	if done {
+		return -1
+	}
+	return min
 }
 
 type Node struct {
-	Name  string
-	Left  string
-	Right string
+	Name      string
+	LeftName  string
+	RightName string
+	Left      *Node
+	Right     *Node
+	Terminal  bool
 }
+
+const data string = ""
 
 const sample string = `RL
 
@@ -67,12 +120,21 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)`
 
-func getInput(t InputType) []string {
+const sample3 string = `LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`
+
+func getInput(t string) []string {
 	var input string
-	if t == Sample {
-		input = sample
-	} else if t == Sample2 {
-		input = sample2
+	if t != "" {
+		input = t
 	} else {
 		if dat, err := os.ReadFile("input.txt"); err == nil {
 			input = string(dat)
