71c192d7fdcf6905b97ea2e002b6d2a2f57b46e3
Emilio Lahr-Vivaz <elahrvivaz@ccri.com>
2024-12-17 09:28:46 -0500

    day 13 part 2

--- 2024/scala/src/main/scala/aoc/day14/Day14.scala
+++ 2024/scala/src/main/scala/aoc/day14/Day14.scala
@@ -4,7 +4,7 @@ import aoc.Aoc
 
 import scala.collection.mutable.ArrayBuffer
 
-object Day13 extends Aoc {
+object Day14 extends Aoc {
 
   override def part1(lines: List[String]): String = {
     val robots = lines.map(Robot.apply)
@@ -21,7 +21,17 @@ object Day13 extends Aoc {
   }
 
   override def part2(lines: List[String]): String = {
-    ""
+    val robots = lines.map(Robot.apply)
+    if (robots.lengthCompare(20) < 0) {
+      // sample
+      ""
+    } else {
+      val grids = Range(1, 10000).foldLeft(Seq(0 -> Grid(101, 103, robots))) { case (grids, i) => grids :+ i -> grids.last._2.moveRobots(1) }
+      val (i, grid) = grids.minBy(_._2.adjacencyScore)
+      grid.printGrid()
+      i.toString
+    }
+
   }
 
   case class Robot(px: Int, py: Int, vx: Int, vy: Int) {
@@ -35,7 +45,9 @@ object Day13 extends Aoc {
   }
 
   case class Grid(x: Int, y: Int, robots: Seq[Robot]) {
+
     def moveRobots(secs: Int): Grid = Grid(x, y, robots.map(_.move(secs, x, y)))
+
     def robotsPerQuadrant: Seq[Int] = {
       // note: assumes x and y are odd
       val midX = x / 2
@@ -54,12 +66,62 @@ object Day13 extends Aoc {
       }
       Seq(c0, c1, c2, c3)
     }
+
+    lazy val adjacencyScore: Int = {
+      val grid = Array.fill(x)(Array.fill(y)(false))
+      robots.foreach(r => grid(r.px)(r.py) = true)
+      var adjacent = 0
+      var j = 1
+      while (j < y - 1) {
+        var i = 1
+        while (i < x - 1) {
+          if (grid(i)(j) &&
+            grid(i-1)(j) || grid(i+1)(j) || grid(i)(j-1) || grid(i)(j+1) ||
+            grid(i-1)(j-1) || grid(i+1)(j-1) || grid(i-1)(j+1) || grid(i+1)(j+1)) {
+            adjacent += 1
+          }
+          i += 1
+        }
+        j += 1
+      }
+      adjacent
+    }
+
+//    def runs(): Int = {
+//      val grid = Array.fill(x)(Array.fill(y)(false))
+//      robots.foreach(r => grid(r.px)(r.py) = true)
+//
+//      var hasRobot = false
+//      var runs = 0
+//      var j = 0
+//      while (j < y) {
+//        var i = 0
+//        while (i < x) {
+//          if (hasRobot != grid(i)(j)) {
+//            hasRobot = !hasRobot
+//            runs += 1
+//          }
+//          i += 1
+//        }
+//        j += 1
+//      }
+//      runs
+//    }
+//
+//    def mightBeATree(): Boolean = {
+//      val midX = x / 2
+//      val midY = y / 2
+//      def hasRobot(xy: (Int, Int)): Boolean = robots.exists(r => r.px == midX + xy._1 && r.py == midY + xy._2)
+//      Seq((0, -1), (0, 0), (0, 1), (-1, 0), (-1, -1), (-1, 1), (1, 0), (1, -1), (1, 1)).count(hasRobot) > 7
+//    }
+
     def printGrid(): Unit = {
       var j = 0
       while (j < y) {
         var i = 0
         while (i < x) {
-          print(robots.count(r => r.px == i && r.py == j))
+          val count = robots.count(r => r.px == i && r.py == j)
+          print(if (count == 0) '.' else count)
           i += 1
         }
         println
